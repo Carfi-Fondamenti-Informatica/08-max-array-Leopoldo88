b88bcdecf32ad0ea591d8ab2c8b6d5db2b046e4a
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-12 21:11:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-#include <iostream>
-#include"lib.h"
-using namespace std;
-
 int main(){
 int n;
 cin>>n;
 int a[n];
 for(int i=0,i<n,i++)
 cin<<a[i];
-max(a[n]);
-  return 0;
+max(n, a[]);
+return 0;
 }
