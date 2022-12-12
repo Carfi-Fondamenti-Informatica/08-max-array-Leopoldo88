899c1f277e29670cc3cc728a1a82132ef2c87e9c
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-12 20:51:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+void max(int v[N]){
+int min;
+int temp;
+for(int i=0;i<N-1;i++)
+        {
+            min=i;
+            for(int j=i+1;j<N;j++)
+                {
+                    if(v[min]>v[j])
+                        min=j;
+                    temp=v[min];
+                    v[min]=v[i];
+                    v[i]=temp;
+                    count++;
+                }
+        }
+ cout<<v[N-1] endl;
+}
